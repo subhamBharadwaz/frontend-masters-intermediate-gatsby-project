118de81f2c00ce2930614e386f0ed43d4682f4d8
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-08-14 14:40:50 +0530

    add sahred nav

---
+++ gatsby-theme-shared-nav/src/components/layout.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Nav from './nav';
+import '../styles/variables.css';
+import '../styles/global.css';
+import { content, footer } from '../styles/layout.module.css';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Nav />
+      <main className={content}>{children}</main>
+      <footer className={footer}>Build with the Shared Nav Gatsby Theme</footer>
+    </>
+  );
+}
---
+++ gatsby-theme-shared-nav/src/components/nav.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { useStaticQuery, Link, graphql } from 'gatsby';
+import { container, sharedNav, link } from '../styles/nav.module.css';
+
+export default function Nav() {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          navItems {
+            label
+            path
+          }
+        }
+      }
+    }
+  `);
+
+  const navItems = data.site.siteMetadata.navItems;
+  return (
+    <header className={container}>
+      <Link to="/" className={link}>
+        {data.site.siteMetadata.title}
+      </Link>
+      <nav className={sharedNav}>
+        {navItems.map((item) => (
+          <Link key={`nav-${item.path}`} to={item.path} className={link}>
+            {item.label}
+          </Link>
+        ))}
+      </nav>
+    </header>
+  );
+}
